e0155e259e4756e4a6398e494d1e357107796cf5
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2022-07-12 12:27:12 -0300

    Testing recorder

--- README.md
+++ README.md
@@ -2,15 +2,11 @@
 
 A new Flutter project.
 
-## Getting Started
+### Dependencies
 
-This project is a starting point for a Flutter application.
+Install:
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```sh
+flutter pub add permission_handler
+flutter pub add flutter_sound_lite
+```
--- lib/api/sound_recorder.dart
+++ lib/api/sound_recorder.dart
@@ -1,16 +1,40 @@
 import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-final pathToSaveFile = "whistle.aac"
+final pathToSaveFile = "whistle.aac";
 
 class SoundRecorder {
   FlutterSoundRecorder? _audioRecorder;
 
+  bool get isRecording => _audioRecorder!.isRecording;
+
+  Future init() async {
+    _audioRecorder = FlutterSoundRecorder();
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException("Microphone permission required");
+    }
+    await _audioRecorder!.openAudioSession();
+  }
+
+  Future dispose() async {
+    await _audioRecorder!.closeAudioSession();
+    _audioRecorder = null;
+  }
+
   Future _record() async {
-    await _audioRecorder!.startRecorder(toFile: pathToSaveFile );
+    await _audioRecorder!.startRecorder(toFile: pathToSaveFile);
   }
 
   Future _stop() async {
     await _audioRecorder!.stopRecorder();
   }
 
+  Future toggleRecording() async {
+    if (_audioRecorder!.isStopped) {
+      await _record();
+    } else {
+      await _stop();
+    }
+  }
 }
